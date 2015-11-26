77d28e3d5cacc1ace34dc37115827b9e189f52f1
etienneg86 <etiennegonnu@gmail.com>
2015-11-26 12:02:02 +0000

    Web cv

---
+++ js/scrollmovement.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+     $('a[href^="#"]').on('click', function (e) {
+         e.preventDefault();
+
+         var target = this.hash,
+             $target = $(target);
+         var position = $target.offset().top - 50
+
+         $('html, body').stop().animate({
+             scrollTop: position // 60 c'est la height de ma navbar en pixels
+         }, 600, 'swing' );
+     });
+ });
